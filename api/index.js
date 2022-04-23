a201267138b9d2a29969c6b6e79852d770254ccb
RoxZkiL <mttrox@gmail.com>
2022-04-23 19:16:38 -0400

    make it better

--- api/index.js
+++ api/index.js
@@ -23,6 +23,6 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3000"); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
