7b9bdbe86b417e72d48ca7df4a783668496a1161
RoxZkiL <mttrox@gmail.com>
2022-05-24 13:56:25 -0400

    fix

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -6,11 +6,19 @@ import style from "./About.module.css";
 export default function About() {
   return (
     <div className={style.div}>
-      <a href="https://github.com/RoxZkiL">
+      <a
+        href="https://github.com/RoxZkiL"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img className={style.img} src={logo2} alt="Not found" />
       </a>
       <div className={style.div2}>
-        <a href="https://www.linkedin.com/in/jes%C3%BAs-matute-079b0b209/">
+        <a
+          href="https://www.linkedin.com/in/jes%C3%BAs-matute-079b0b209/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={logo} alt="Not found" />
         </a>
       </div>
